ce68ee337359f84a818cad88488bdf0b2f2fc1b3
LTP <lamtruongphu2003@gmail.com>
2024-12-26 01:58:31 +0700

    layout profile page

--- src/app/(client)/profile/page.tsx
+++ src/app/(client)/profile/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function ProfilePage() {
-    return <div className="w-full max-w-7xl mx-auto">ProfilePage</div>;
+    return <div className="w-full max-w-4xl lg:max-w-7xl mx-auto py-10 ">
+
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-12">
+            <div className="lg:col-span-1 h-screen">
+                <div className="h-full w-full bg-black"></div>
+            </div>
+            <div className="lg:col-span-2 h-screen">
+                <div className="h-full w-full bg-gray-500"></div>
+
+            </div>
+        </div>
+        ProfilePage
+    </div>;
 }
